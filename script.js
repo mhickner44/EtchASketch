c3512df42bbc88f37eb28177da53245759aedd4d
mhickner44 <rmicu1903@gmail.com>
2023-01-13 09:09:10 -0500

    working color selectors

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const pad = document.querySelector(".pad");
 const slider = document.querySelector(".slider");
+const clear = document.getElementById("clear");
+const picker = document.getElementById("colorPicker");
+const rgb = document.getElementById("RGB");
+const eraser = document.getElementById("eraser");
+
+
+let colorValue = "white";
+
 let mouseDown = false;
 
 
 let gridSize = 16;
 
-//
+
 
 
 
 function drawGrid() {
     gridSize = slider.value;
-    
+
     pad.innerHTML = '';
     pad.style.cssText = `grid-template-rows:repeat(${gridSize},1fr);grid-template-columns:repeat(${gridSize},1fr);`
 
@@ -21,11 +29,11 @@ function drawGrid() {
         const block = document.createElement("div");
         pad.appendChild(block);
         block.classList.add("block");
-        
+
         block.addEventListener("mouseover", event => {
             if (mouseDown) {
                 block.classList.add("selected")
-
+                block.style.cssText = `background-color:${colorValue}`;
             }
         }
         );
@@ -51,7 +59,46 @@ document.body.onmouseup = function () {
 
 
 
+function changeColor() {
+    //get the color from the picker
+    colorValue = picker.value;
+    pad.removeEventListener("mousemove", rgbColor);
+}
+function eraserColor() {
+    //get the color from the picker
+    colorValue = "lightgray";
+    pad.removeEventListener("mousemove", rgbColor);
+}
+
+function rainbow() {
+    //get the color from the picker
+    pad.addEventListener("mousemove", rgbColor);
+
+}
+
+function rgbColor() {
+    let red, green, blue;
+
+    red = Math.random() * 255;
+    blue = Math.random() * 255;
+    green = Math.random() * 255;
+
+    colorValue = `rgb(${red},${blue},${green})`;
+
+
+}
+
+
+
 //decide between input and change
 drawGrid();
+
 slider.addEventListener("change", drawGrid);
+//color  choices
+clear.addEventListener("click", drawGrid);
+rgb.addEventListener("click", rainbow);
+picker.addEventListener("change", changeColor);
+eraser.addEventListener("click", eraserColor);
+
+
 
